7f0b6e1dbf8f9a2d5ed24054c591a7a1a69218ba
OlgaKuvs <olga_kuv@inbox.lv>
2023-08-16 22:43:21 +0100

    create add tasks page

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -3,3 +3,24 @@ document.addEventListener("DOMContentLoaded", function() {
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    M.Modal.init(elems);
+  });
+
+  // datepicker initialization
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+  });
+
+  // select initialization
+
+  var selects = document.querySelectorAll('select');
+  M.FormSelect.init(selects);
