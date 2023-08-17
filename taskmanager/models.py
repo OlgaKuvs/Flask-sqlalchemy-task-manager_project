3ddee5826f89e65c5c1d51cc7048882899c983c1
OlgaKuvs <olga_kuv@inbox.lv>
2023-08-17 16:35:13 +0100

    preparing for deployment

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -5,6 +5,7 @@ class Category(db.Model):
     # schema for the Category model
     id=db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
+    # cascade effect to delete all tasks for deleted category
     tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
 
     def __repr__(self):
